ec24649d1bb230c0eda76b3c97b689199953723c
William Mallett <mallett002@gmail.com>
2018-08-19 09:31:15 -0500

    Created basic setup for app.

---
+++ src/containers/Root.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+
+// Import other container components that Root will render based on Route URL
+import Home from './Home' 
+
+class Root extends Component {
+    render() {
+        return (
+            <Router>
+                <Route path='/' component={Home} />
+            </Router>
+        )
+    }
+
+}
+
+
+Root.proptypes = {
+    store: PropTypes.object.isRequired
+}
+
+export default Root
