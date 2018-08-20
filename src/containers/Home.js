0a467de41f198004d630ab80e7607ced5195b255
William Mallett <mallett002@gmail.com>
2018-08-20 11:18:28 -0500

    Updated Url to fetch Reddit News Data

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,8 +2,14 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { fetchNews } from '../actions/actions';
 
 class Home extends Component {
+
+    componentDidMount() {
+        const { fetchNews } = this.props;
+        fetchNews();
+    }
 
     renderErrorMessage() {
         const { errorMessage } = this.props
@@ -27,10 +33,18 @@ class Home extends Component {
     }
 }
 
+Home.Proptypes = {
+    fetchNews: PropTypes.func.isRequired,
+    isLoading: PropTypes.bool.isRequired,
+    errorMessage: PropTypes.string,
+    data: PropTypes.object.isRequired,
+};
 
 
 const mapStateToProps = (state) => ({
+    isLoading: state.isLoading,
     errorMessage: state.errorMessage,
-})
+    data: state.data
+});
 
-export default withRouter(connect(mapStateToProps)(Home))
+export default withRouter(connect(mapStateToProps, { fetchNews })(Home));
