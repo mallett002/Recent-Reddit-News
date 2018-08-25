3a158d237da0143f924281f9a6a5e3afd62a5677
William Mallett <mallett002@gmail.com>
2018-08-25 09:38:43 -0500

    Made header buttons dynamically change according to selected and added dynamic hr

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import NewsType from './NewsType';
 
-const Header = ({ handleFetch }) => (
-        <div className='nav'>
-            <NewsType child='US News' handleFetch={handleFetch} category='usNews' buttonColor='#2196f3' />
-            <NewsType child='Sports' handleFetch={handleFetch} category='sports' buttonColor='#e29f30' />
-            <NewsType child='Politics' handleFetch={handleFetch} category='politics' buttonColor='#70ab70' />
-            <NewsType child='Uplifting' handleFetch={handleFetch} category='uplifting' buttonColor='#ad5454' />
-        </div>
-);
+const hrStyles = {
+    usnews: { background: '#2196f3' },
+    sports: { background: '#e29f30' },
+    politics: { background: '#70ab70' },
+    uplifting: { background: '#ad5454' },
+};
+
+class Header extends Component {
+    state = {
+        selected: null
+    }
+
+    handleSelected = (category) => {
+        this.setState({ selected: category});
+    }
+    
+    render() {
+        const { handleFetch } = this.props;
+        const { selected } = this.state;
+        return (
+            <div className='header'>
+                <div className='nav'>
+                    <NewsType child='US News' handleFetch={handleFetch} category='usNews' buttonColor='#2196f3' handleClick={this.handleSelected} selected={selected} />
+                    <NewsType child='Sports' handleFetch={handleFetch} category='sports' buttonColor='#e29f30' handleClick={this.handleSelected} selected={selected} />
+                    <NewsType child='Politics' handleFetch={handleFetch} category='politics' buttonColor='#70ab70' handleClick={this.handleSelected} selected={selected} />
+                    <NewsType child='Uplifting' handleFetch={handleFetch} category='uplifting' buttonColor='#ad5454' handleClick={this.handleSelected} selected={selected} />
+                </div>
+                <hr className='header-hr'
+                    style={Object.assign({}, 
+                        selected === 'usNews' && hrStyles.usnews,
+                        selected === 'sports' && hrStyles.sports,
+                        selected === 'politics' && hrStyles.politics,
+                        selected === 'uplifting' && hrStyles.uplifting
+                    )} 
+                />
+            </div>
+        )
+    }
+}
+
 
 Header.propTypes = {
     handleFetch: PropTypes.func.isRequired
